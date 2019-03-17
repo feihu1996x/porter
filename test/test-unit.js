e2b675981842b268e35ad581e664761789add1db
feihu1996x <gsx0312@qq.com>
2019-03-17 17:34:25 +0800

    update

--- test/test-unit.js
+++ test/test-unit.js
@@ -3,12 +3,35 @@ process.env.NODE_ENV = 'test';
 const uuidv4 = require('uuid/v4');
 const chai = require('chai');
 const path = require('path');
+const fs = require('fs');
+const request = require('request');
+
+const lib = require('../lib');
 
 chai.should();
 
 describe('lib.ClearDir() tests', function () {
     const target_path = path.resolve(__dirname, uuidv4());
-    it('should clear the specified directory', () => {
-        
+    it('should clear the specified directory', (done) => {
+        fs.mkdir(target_path, { recursive: true }, (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                request('https://img1.mukewang.com/szimg/59c860220001247d05400300-360-202.jpg')
+                    .pipe(
+                        fs.createWriteStream(path.resolve(target_path, 'test.jpg'))
+                    )
+                    .on('close', () => {
+                        lib.ClearDir(target_path);
+                        fs.rmdir(target_path, (err) => {
+                            if (err) {
+                                console.error(err)
+                            }
+                            Boolean(err).should.equal(false)
+                        })
+                    });
+            }
+            done()
+        });
     }).timeout(100000);
 });
